998279625f87c4ef6a24b1b442a897ccb238959a
Mayank mishra <90859312+Mayank1242@users.noreply.github.com>
2022-05-08 10:46:52 -0700

    vcg

---
+++ Java_Lab_workplace/src/Assignment_01/Q1.java
@@ -0,0 +1,57 @@
+package Assignment_01;
+
+public class Q1  {
+
+	public static void main(String[] args) {
+		System.out.println("Question 01");
+		System.out.println("vansh_F_20011491");
+		Student obj = new Student("Rahul", 14, 'A', 88);
+		Student obj1 = new Student("Sumit", 14, 'A', 78);
+		Student obj2 = new Student("YAsh", 12, 'A', 98);
+		Student obj3 = new Student("Abhishek", 15, 'A', 38);
+		Student obj4 = new Student("Aniket", 69, 'A', 58);
+		Student obj5 = new Student("Pulkit", 18, 'A', 98);
+		//Getting the result 
+		Student result = new Student();
+		result.get_avg();
+		
+
+	}
+
+}
+
+class Student
+{
+	String name;
+	int age;
+	char Section;
+	int percentage; //assuming percentage to be discrete value
+	static int avg_per = 0;
+	
+	Student()
+	{
+		age = 0;
+		percentage = 0;
+	}
+	Student(String s, int a, char sec, int per) // Constructor name should be equal to class name
+	{
+		name = s;
+		age = a;
+		Section = sec;
+		percentage = per;
+		avg_per += per;
+		
+		System.out.println("\t");
+		System.out.println("Name: "+name);
+		System.out.println("Section= "+Section);
+		System.out.println("Age= "+age);
+		System.out.println("Percentage= "+percentage+"%");
+	}
+	
+	void get_avg()
+	{
+		int average = avg_per/6;
+		System.out.println("\t");
+		System.out.println("Average Percantage of all students= "+average+"%"); 
+	}
+}
---
+++ Java_Lab_workplace/src/Assignment_01/Q8.java
@@ -0,0 +1,39 @@
+package Assignment_01;
+
+import java.util.*;
+
+public class Q8 {
+	
+		public static void main(String[] agrs) {
+			
+			System.out.println("Question 08");
+			System.out.println("vansh agarwal_F_20011491");
+			System.out.println("\t");
+			
+			Scanner in=new Scanner(System.in);
+			System.out.print("Enter the size of array:");
+			int n=in.nextInt();
+			int arr[]=new int[n];
+			
+			System.out.println("Enter the Element of array:");
+			for(int i=0;i<n;i++) {
+				arr[i]=in.nextInt();
+			}
+			
+			int m;
+			System.out.println("Enter the index whose element is to be deleted");
+			m=in.nextInt();
+			
+			System.out.println("Deleted Element="+arr[m]);
+			
+			for(int i=m;i<n-1;i++) {
+				arr[i]=arr[i+1];
+			}
+			
+			System.out.println("Array after the number is deleted");
+			for(int i=0;i<n-1;i++)
+			{
+				System.out.print(arr[i]+" ");
+			}
+		}
+	}
