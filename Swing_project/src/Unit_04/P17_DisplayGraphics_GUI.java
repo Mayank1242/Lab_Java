e042bbb8ccf56530a1f834e479a24ac91036b6d1
Mayank mishra <90859312+Mayank1242@users.noreply.github.com>
2022-06-02 21:26:59 -0700

    commit

--- Swing_project/src/Unit_04/P17_DisplayGraphics_GUI.java
+++ Swing_project/src/Unit_04/P17_DisplayGraphics_GUI.java
@@ -1,12 +1,29 @@
 package Unit_04;
 
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
 
 public class P17_DisplayGraphics_GUI  extends Canvas{
 	
 	public static void main(String[]  args) {
 		P17_DisplayGraphics_GUI obj=new P17_DisplayGraphics_GUI();
 		
+	  JFrame f=new JFrame();
+	  f.add(obj);
+	  f.setSize(500,500);
+		f.setVisible(true);
 	}
-
+    
+	public void paint(Graphics grap) {
+	grap.drawOval(25, 140, 40, 70);	
+	setForeground(Color.gray);
+	grap.fillOval(140,140,40,60);
+	
+	grap.drawArc(10,20,140,150,90,60);
+	
+	}
+	
 }
